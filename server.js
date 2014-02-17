91622752567d6b5379e92fb9ca4b2464ac62f084
vagrant <vagrant@precise32.(none)>
2014-02-17 05:59:24 +0000

    Heroku Commit

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.engine('html', require('ejs').renderFile);
 
 
 
-var port = 8000;
-app.listen(8000);
+var port = Number(process.env.PORT || 8000);
+app.listen(port;
 console.log("App listening on port: " + port);
