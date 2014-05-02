42fd46b425a3efa56bfd13f6b441f02b84207921
vagrant <vagrant@precise32.(none)>
2014-05-02 04:31:00 +0000

    Commit

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
 	var handlers = require('./handlers');
 	app.get('/', handlers.index);
-	app.get('/music', handlers.watch)
+	app.get('/music', handlers.watch);
+	app.get('/boostrap', handlers.boostrap);
 }
